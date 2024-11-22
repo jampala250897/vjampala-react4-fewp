6e80288e3399f439d83b6b51871e82177b1f2f5c
VINEETH KUMAR JAMPAL <jampala250897@gmail.com>
2024-11-21 23:36:30 -0600

    final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import UserList from "./components/UserList";
+import AddUser from "./components/AddUser";
+import EditUser from "./components/EditUser";
+import UserDetails from "./components/UserDetails";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="users" element={<UserList />}>
+          <Route path=":userId" element={<UserDetails />} />
+          <Route path="edit/:userId" element={<EditUser />} />
+        </Route>
+        <Route path="add-user" element={<AddUser />} />
+      </Routes>
+    </Router>
   );
 }
 
